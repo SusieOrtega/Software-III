2363464b045ec1e3d473b488d2589013acc8f090
Susi Ortega <suortort10@gmail.com>
2018-03-07 11:25:29 -0600

    Estructuras
    de control.. If

---
+++ Estructuras de Control.R
@@ -0,0 +1,15 @@
+#Sintaxis
+if(condición)
+{#algunas instrucciones
+}
+else
+{##Algunas otras instrucciones
+}
+
+x=5
+if(x>=5)
+{
+  "El número es mayor que 5."
+  }else
+{"El número es menor que 5."
+  }
