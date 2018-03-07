a7e55a59b00bbaefd60fcb8d98bc02a71fe465a4
Susi Ortega <suortort10@gmail.com>
2018-03-07 11:42:54 -0600

    2
    3

--- Estructuras de Control.R
+++ Estructuras de Control.R
@@ -1,8 +1,7 @@
 #Sintaxis
 if(condición)
 {#algunas instrucciones
-}
-else
+}else
 {##Algunas otras instrucciones
 }
 
@@ -12,4 +11,28 @@ if(x>=5)
   "El número es mayor que 5."
   }else
 {"El número es menor que 5."
-  }
+}
+
+#IRONMAN
+x=7
+if (x<=5){
+  print("Menor a 5")
+}else if (x<=10){
+  print("entre 5 y 10")
+}else{
+  print("Mayor a 10")
+}
+
+if(x>3){
+  y <- 10
+}else{
+  y <- 0
+}
+y
+
+y <- if (x>3){
+  10
+}else{
+  0
+}
+y
