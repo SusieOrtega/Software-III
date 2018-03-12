8293c8f87f390a4e47d851558f6ec758e557ffe5
Susi Ortega <suortort10@gmail.com>
2018-03-12 11:49:53 -0600

    Clse 12-03
    For & While

---
+++ Clase12-03.R
@@ -0,0 +1,56 @@
+#Ciclos For 
+
+for(i in 1:10){
+  print(i)
+}
+
+for(i in c("a", "b","c")){
+  print(i)
+}
+
+#La forma en la que trabaja for dentro de R es mas similar al for each en vba
+
+x <- c("a","b","c","d")
+for(i in 1:4){
+  print(x[i])
+}
+
+for(i in seq_along(x)){
+  print(x[i])
+}
+
+for (letra in x) {
+  print(letra)
+}
+
+for (i in 1:4) print(x[i])
+
+
+#While
+
+count <- 0
+while(count<10){
+  print(count)
+  count <- count +1
+}
+
+#Ejemplo de While
+set.seed(1)
+z <- 5
+contador <- 0
+while (z>=3 && z<=10) {
+  print(z)
+  moneda <- rbinom(1,1,0.5)
+  if(moneda==1){#Caminata Aleatoria
+    z <- z+1
+  } else {
+    z <- z-1
+  }
+  contador <- contador+1
+
+}
+print("Se realizaron"); print(contador); print("pasos")
+
+y <- list(1:contador)
+y[[1]]
+
